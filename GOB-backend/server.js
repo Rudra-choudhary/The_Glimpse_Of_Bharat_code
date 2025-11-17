2520738d1e8f2eaa044a2865a92da8515ef4319b
Rudra Pratap Singh Choudhary <rudra@Rudras-MacBook-Pro.local>
2025-11-17 18:29:30 +0530

    change backend

--- GOB-backend/server.js
+++ GOB-backend/server.js
@@ -39,7 +39,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/scans", scanRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => res.send("NetMapper API running..."));
+app.get("/", (req, res) => res.send("Glimpse of bharat API running..."));
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server working fine on ${PORT}`));
