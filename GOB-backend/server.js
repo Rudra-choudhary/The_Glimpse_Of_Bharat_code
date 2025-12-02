01b8f3f3ba708ec906bb9f789c44ad661d335e30
Rudra Pratap Singh Choudhary <rudra@Rudras-MacBook-Pro.local>
2025-12-02 12:00:59 +0530

    update cors

--- GOB-backend/server.js
+++ GOB-backend/server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 
 // extra CORS headers for preflight
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://the-glimpse-of-bharat-code.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
