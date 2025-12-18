e8cbdb2909c7b354ab4c1ab4954ec90c33049472
Rudra Pratap Singh Choudhary <rudra@Rudras-MacBook-Pro.local>
2025-12-18 23:46:55 +0530

    Add Google site verification meta tag

--- GOB-frontend/pages/_document.js
+++ GOB-frontend/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="virlFrit6bc7vo1X8ILHH8IaXYFJlAjMmh8QWxLvBdE" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
